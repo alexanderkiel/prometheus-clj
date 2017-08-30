21f35d6cf2729f09a092c155b5e8464d9f9092cd
Alexander Kiel <alexanderkiel@gmx.net>
2017-08-30 13:04:09 +0200

    Bump Next Dev Version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.akiel/prometheus-clj.alpha "0.1"
+(defproject org.clojars.akiel/prometheus-clj.alpha "0.2-SNAPSHOT"
   :description "Clojure wrappers for the Prometheus Java client."
   :url "https://github.com/alexanderkiel/prometheus-clj.alpha"
 
